0d2732107607d6aed9610a0271f077eaca3dac03
Jaxon Kade <jobermil05@gmail.com>
2021-07-06 02:59:34 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,21 @@
+# Autobump Selfbot
+# Read the readme.md for detais on setting up
+import discord
+from discord.ext import commands
+
+c = commands.Bot(command_prefix=';', self_bot=False, reconnect=True, status=discord.Status.idle)
+c.remove_command('help')
+token = open("config/token.txt").read()
+
+@c.event
+async def on_ready():
+    print(f'{c.user} is logged in')
+
+@c.event
+async def on_message(ctx):
+    if ctx.author.id == 735147814878969968:
+        await ctx.channel.send('!d bump')
+        pass
+    pass
+
+c.run(token, bot=False)
