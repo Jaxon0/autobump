2208063fe19ef586c2b972a4cd2c46a3ced839af
Jaxon Kade <jobermil05@gmail.com>
2021-07-06 17:02:40 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@
 import discord
 from discord.ext import commands
 import asyncio
+import datetime
+import time
 
 c = commands.Bot(command_prefix=';', self_bot=False, reconnect=True, status=discord.Status.idle)
 c.remove_command('help')
@@ -14,13 +16,14 @@ async def on_ready():
     pass
 
 @c.event
-async def on_message(ctx):
+async def on_message(ctx, limit=1):
     if ctx.author.id == 735147814878969968:
         await ctx.channel.trigger_typing()
         await asyncio.sleep(3)
         await ctx.channel.send('!d bump')
+        await asyncio.sleep(2)
+        await ctx.channel.purge(limit=limit)
         pass
     pass
-
         
 c.run(token, bot=False)
