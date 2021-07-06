396c419b82dab2b678339a5072dbc54ed98630ee
Jaxon Kade <jobermil05@gmail.com>
2021-07-06 04:11:41 -0500

    now types for 3s before sending !d bump, to try and look more "real"

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 # Read the readme.md for detais on setting up
 import discord
 from discord.ext import commands
+import asyncio
 
 c = commands.Bot(command_prefix=';', self_bot=False, reconnect=True, status=discord.Status.idle)
 c.remove_command('help')
@@ -10,12 +11,16 @@ token = open("config/token.txt").read()
 @c.event
 async def on_ready():
     print(f'{c.user} is logged in')
+    pass
 
 @c.event
 async def on_message(ctx):
     if ctx.author.id == 735147814878969968:
+        await ctx.channel.trigger_typing()
+        await asyncio.sleep(3)
         await ctx.channel.send('!d bump')
         pass
     pass
 
+        
 c.run(token, bot=False)
